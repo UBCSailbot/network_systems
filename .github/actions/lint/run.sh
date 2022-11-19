b193c075fa64c59637b344ff63f203cff992d4cb
Henry Huang <69825683+hhenry01@users.noreply.github.com>
2022-11-18 21:36:56 -0800

    Copy sailbot_workspace files (#1)

---
+++ .github/actions/lint/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+source /opt/ros/${ROS2_DISTRO}/setup.bash
+./setup.sh
+if [[ "$LINTER" == "clang-tidy" ]]
+then
+    ./build.sh Debug OFF
+    ./run_clang-tidy.sh
+else
+    ament_${LINTER} src/
+fi
