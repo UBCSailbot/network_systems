b311e9827ddfc051828df48e4498e38db81fe272
DFriend01 <devon.fri.2000@gmail.com>
2023-07-12 23:05:05 -0700

    Rename example.py and fixed invalid Node name-value parameters

--- launch/network_systems_launch.py
+++ launch/network_systems_launch.py
@@ -6,8 +6,8 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='network_systems',
-            node_namespace='example',
-            node_executable='example',
-            node_name='cached_fib'
+            namespace='example',
+            executable='example',
+            name='cached_fib'
         )
     ])
