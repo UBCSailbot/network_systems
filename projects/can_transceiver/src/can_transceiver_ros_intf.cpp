fcbbfe4aae68f71f24d16b56755205cac2a75c1a
Colin Li <colin_bc@hotmail.com>
2023-10-07 15:40:27 -0700

    Added print for all pubsub Noted all known issues in comments and issue page Properly works with ros2 run command & boat sim on

--- projects/can_transceiver/src/can_transceiver_ros_intf.cpp
+++ projects/can_transceiver/src/can_transceiver_ros_intf.cpp
@@ -1,4 +1,6 @@
 #include <cstdio>
+#include <custom_interfaces/msg/detail/batteries__struct.hpp>
+#include <custom_interfaces/msg/detail/generic_sensors__struct.hpp>
 #include <custom_interfaces/msg/detail/gps__struct.hpp>
 #include <custom_interfaces/msg/detail/wind_sensor__struct.hpp>
 #include <custom_interfaces/msg/detail/wind_sensors__struct.hpp>
@@ -13,6 +15,10 @@
 
 #include "custom_interfaces/msg/gps.hpp"
 #include "custom_interfaces/msg/wind_sensors.hpp"
+#include "custom_interfaces/msg/batteries.hpp"
+#include "custom_interfaces/msg/generic_sensors.hpp"
+#include "custom_interfaces/msg/wind_sensor.hpp"
+
 #include "rclcpp/publisher.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/subscription.hpp"
@@ -23,6 +29,8 @@
 using namespace std::chrono_literals;
 
 #define QUEUE_SIZE 10
+#define WIND_SENSOR_BOUND 2
+#define PLACEHOLDER_VALUE 42 // Placeholder value for debugging or testing
 
 class CanTransceiverIntf : public rclcpp::Node
 {
@@ -85,15 +93,15 @@ public:
           "mock_gps", QUEUE_SIZE, std::bind(&CanSimIntf::gps_callback, this, std::placeholders::_1));
 
         // Topic: mock_wind_sensors
-        subscriptionWindSensor_ = this->create_subscription<custom_interfaces::msg::WindSensor>(
+        subscriptionWindSensor_ = this->create_subscription<custom_interfaces::msg::WindSensors>(
           "mock_wind_sensors", QUEUE_SIZE, std::bind(&CanSimIntf::wind_callback, this, std::placeholders::_1));
 
     // Publisher
-        publisher1wind_sensors_ = this->create_publisher<std_msgs::msg::String>("wind_sensors", QUEUE_SIZE);
-        publisher2filtered_wind_sensor_ = this->create_publisher<std_msgs::msg::String>("filtered_wind_sensor", QUEUE_SIZE);
-        publisher3gps_ = this->create_publisher<std_msgs::msg::String>("gps", QUEUE_SIZE);
-        publisher4data_sensors_ = this->create_publisher<std_msgs::msg::String>("data_sensors", QUEUE_SIZE);
-        publisher5batteries_ = this->create_publisher<std_msgs::msg::String>("batteries", QUEUE_SIZE);
+        publisherWindSensors_ = this->create_publisher<custom_interfaces::msg::WindSensors>("wind_sensors", QUEUE_SIZE);
+        publisherWindSensor_ = this->create_publisher<custom_interfaces::msg::WindSensor>("filtered_wind_sensor", QUEUE_SIZE);
+        publisherGPS_ = this->create_publisher<custom_interfaces::msg::GPS>("gps", QUEUE_SIZE);
+        publisherGenericSensors_ = this->create_publisher<custom_interfaces::msg::GenericSensors>("data_sensors", QUEUE_SIZE);
+        publisherBatteries_ = this->create_publisher<custom_interfaces::msg::Batteries>("batteries", QUEUE_SIZE);
         // Timer with 500ms delay
         timer_     = this->create_wall_timer(500ms, std::bind(&CanSimIntf::timer_callback, this));
     }
@@ -102,59 +110,94 @@ private:
 
 // Subscriber
 // When moment message is available in queue: prints statement in log
+// Reminder: If publisher node (boat_simulator isn't running, this will not print.)
     void gps_callback(const custom_interfaces::msg::GPS::SharedPtr msg) const
     {
-        RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->heading.heading);
+        RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->heading.heading); //TO-DO: Combine into 1 print
         RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->lat_lon.latitude);
         RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->lat_lon.longitude);
         RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->speed.speed);
     }
-    void wind_callback(const custom_interfaces::msg::WindSensor::SharedPtr msg) const
+    void wind_callback(const custom_interfaces::msg::WindSensors::SharedPtr msg) const
     {
-        RCLCPP_INFO(this->get_logger(), "I heard: '%d'", msg->direction);
-        RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->speed.speed);
+        for(int i=0;i<WIND_SENSOR_BOUND;++i){
+        RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->wind_sensors[i].speed.speed); //TO-DO: Combine into 1 ()
+        RCLCPP_INFO(this->get_logger(), "I heard: '%i'", msg->wind_sensors[i].direction);
+        }
     }
 
 // Publisher
     void timer_callback()
     {
-        // Publishes to local pathfinding gps through publisher3gps_
-        // lat_lon (lat, lon), speed (kmph), heading (0 to 360 deg)
-        auto message3 = std_msgs::msg::String();
-        message3.data = "Hello, world! " + std::to_string(count_++);  //placeholder helloworld
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message3.data.c_str());
+        // ** GPS **
+        auto gpsPubData = custom_interfaces::msg::GPS();
+        gpsPubData.heading.heading = PLACEHOLDER_VALUE;
+        gpsPubData.speed.speed = PLACEHOLDER_VALUE;
+        gpsPubData.lat_lon.latitude = PLACEHOLDER_VALUE;
+        gpsPubData.lat_lon.longitude = PLACEHOLDER_VALUE;
+        // Log
+        RCLCPP_INFO(this->get_logger(), "GPS Pub: '%f' '%f' '%f' '%f' ",
+        gpsPubData.heading.heading, gpsPubData.speed.speed,  gpsPubData.lat_lon.latitude, gpsPubData.lat_lon.longitude);
         // Publishes msg
-        publisher3gps_->publish(message3);
+        publisherGPS_->publish(gpsPubData);
 
-        // Publishes to publisher1_
-        auto message1 = std_msgs::msg::String();
-        message1.data = "Hello, world! " + std::to_string(count_++);  //placeholder helloworld
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message1.data.c_str());
+        // ** Wind Sensor (WSensor)**
+        auto WSensorData = custom_interfaces::msg::WindSensor();
+        WSensorData.speed.speed = PLACEHOLDER_VALUE;
+        WSensorData.direction = PLACEHOLDER_VALUE;
+        // Log
+        RCLCPP_INFO(this->get_logger(), "Wind Sensor Pub: '%f' '%d'",
+        WSensorData.speed.speed, WSensorData.direction);
         // Publishes msg
-        publisher1wind_sensors_->publish(message1);
+        publisherWindSensor_->publish(WSensorData);
 
-        // Publishes to publisher2_ (placeholder example of repeat)
-        auto message2 = std_msgs::msg::String();
-        message2.data = "Hello, world (again)! " + std::to_string(count_++);  //placeholder helloworld
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message2.data.c_str());
+        // ** Wind Sensors (WSensors)**
+        auto WSensorsData = custom_interfaces::msg::WindSensors();
+        // Note: for loop causes stuck here, maybe have to do separate timer_callback
+        // OR just hard code [1]. [2], .... etc.
+        WSensorsData.wind_sensors[0].direction = PLACEHOLDER_VALUE;
+        WSensorsData.wind_sensors[0].speed.speed = PLACEHOLDER_VALUE;
+        RCLCPP_INFO(this->get_logger(), "Wind Sensors Pub: '%d' '%f'",
+        WSensorsData.wind_sensors[0].direction, WSensorsData.wind_sensors[0].speed.speed);
         // Publishes msg
-        publisher2filtered_wind_sensor_->publish(message2);
+        publisherWindSensors_->publish(WSensorsData);
+
+        // ** Batteries **
+        auto WBatteriesData = custom_interfaces::msg::Batteries();
+        WBatteriesData.batteries[0].voltage = PLACEHOLDER_VALUE;
+        WBatteriesData.batteries[0].current = PLACEHOLDER_VALUE;
+        RCLCPP_INFO(this->get_logger(), "Batteries Pub: '%f' '%f'",
+        WBatteriesData.batteries[0].voltage, WBatteriesData.batteries[0].current);
+        // Publishes msg
+        publisherBatteries_->publish(WBatteriesData);
+
+        // // ** Generic Sensors **
+        // auto GenSensorData = custom_interfaces::msg::GenericSensors();
+        // GenSensorData.generic_sensors[0].id = 0x0; //uint8
+        // GenSensorData.generic_sensors[0].data = 0x0; //uint64
+        // // Publishes msg
+        // publisherGenericSensors_->publish(GenSensorData);
+
+        //I think '%lu' is causing seg err since data is uint64, and id is uint8. Removed print for now.
+        // Debug why segmentation error for this. Guessing .id and .data cannot be done like this.
+
     }
+
 // Field Operations
 
     // Publisher Field Declarations
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher1wind_sensors_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher2filtered_wind_sensor_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher3gps_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher4data_sensors_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher5batteries_;
+    rclcpp::Publisher<custom_interfaces::msg::WindSensors>::SharedPtr publisherWindSensors_;
+    rclcpp::Publisher<custom_interfaces::msg::WindSensor>::SharedPtr publisherWindSensor_;
+    rclcpp::Publisher<custom_interfaces::msg::GPS>::SharedPtr publisherGPS_;
+    rclcpp::Publisher<custom_interfaces::msg::GenericSensors>::SharedPtr publisherGenericSensors_;
+    rclcpp::Publisher<custom_interfaces::msg::Batteries>::SharedPtr publisherBatteries_;
 
     // Subscriber Field Declarations
     rclcpp::Subscription<custom_interfaces::msg::GPS>::SharedPtr subscriptionGPS_;
-    rclcpp::Subscription<custom_interfaces::msg::WindSensor>::SharedPtr subscriptionWindSensor_;
+    rclcpp::Subscription<custom_interfaces::msg::WindSensors>::SharedPtr subscriptionWindSensor_;
 
     // Variable to count number of msgs published
-    size_t                                              count_;
+    //size_t                                              count_;
 
     // Timer object to allow our CamSimIntfFeedback node to perform action at x rate
     rclcpp::TimerBase::SharedPtr                        timer_;
@@ -165,6 +208,8 @@ private:
 //===========================================================================================
 // Main
 //===========================================================================================
+// Run with:
+//$ ros2 run network_systems can_transceiver
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
