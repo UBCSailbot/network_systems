cf312cbe84a0af3a205f0dfa74ff71c45ed70ff2
Sam Dai <daisam1215@gmail.com>
2024-02-14 20:39:46 -0800

    empty http request object according to official example

--- projects/remote_transceiver/src/remote_transceiver.cpp
+++ projects/remote_transceiver/src/remote_transceiver.cpp
@@ -85,7 +85,7 @@ void Listener::run()
 void HTTPServer::readReq()
 {
     std::shared_ptr<HTTPServer> self = shared_from_this();
-    req_.clear();
+    req_                             = {};
     http::async_read(socket_, buf_, req_, [self](beast::error_code e, std::size_t /*bytesTransferred*/) {
         if (!e) {
             self->processReq();
